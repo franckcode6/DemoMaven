c10f45a4af90a3260869d951675a9dd2932491e7
Franckcode6 <franckcode6@gmail.com>
2022-04-13 15:49:17 +0200

    Correction GarageServlet

---
+++ src/main/java/services/GarageService.java
@@ -0,0 +1,16 @@
+package services;
+
+
+import java.sql.*;
+
+public class GarageService {
+    private Connection connexion;
+
+//    public GarageService() {
+//        try {
+//            this.connexion = DbConnexion.getConnexion();
+//        } catch (SQLException | ClassNotFoundException e) {
+//            e.printStackTrace();
+//        }
+//    }
+}
